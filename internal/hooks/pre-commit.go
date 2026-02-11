3152751d3a9fa8933553dca8fcc7aff9f0f337b9
Asher Samuel <ashersam116@gmail.com>
2026-02-11 19:05:58 +0300

    pre-commit: nameing convention for files and folders

--- internal/hooks/pre-commit.go
+++ internal/hooks/pre-commit.go
@@ -8,6 +8,7 @@ import (
 	"path/filepath"
 	"slices"
 	"strings"
+	"unicode"
 
 	"github.com/devasherr/gitlang/internal/config"
 )
@@ -25,6 +26,7 @@ func PreCommit(cfg config.PreCommit) error {
 
 	var errs []error
 
+	folderDirSet := map[string]bool{}
 	for path := range strings.SplitSeq(strings.TrimRight(string(output), "\n"), "\n") {
 		fileInfo, err := os.Stat(path)
 		if err != nil {
@@ -47,9 +49,58 @@ func PreCommit(cfg config.PreCommit) error {
 			ext = ".exe"
 		}
 
+		// TODO: add .env and other sensitive entension here
+		// since hashmap will be used no need to worry about creating duplicates
+
 		if len(cfg.ForbiddenExtensions) > 0 {
+			// TODO: replace with hashmap for faster lookup
+			// also for removing duplicate extensions that might have been given
 			if ext != "" && slices.Contains(cfg.ForbiddenExtensions, ext) {
 				errs = append(errs, fmt.Errorf("%s extension is forbidden", ext))
+			}
+		}
+
+		errs = append(errs, handleNamingConventions(fileInfo.Name(), cfg.NamingConventions.File))
+		folderName := strings.TrimRight(path, fileInfo.Name())
+		if !folderDirSet[folderName] {
+			folderDirSet[folderName] = true
+			errs = append(errs, handleNamingConventions(folderName, cfg.NamingConventions.Folder))
+
+			// feature or bug ??
+			// Top/nested/ <- only lowercase letters allowed
+			// Top/nested/deeper <- only lowercase letters allowed
+			// should it only fail/warn once since Top is the root cause, or warn on every folder path
+		}
+	}
+
+	return errors.Join(errs...)
+}
+
+func checkNoSpace(s string) error {
+	if strings.Contains(s, " ") {
+		return fmt.Errorf("%s: spaces are not allowed", s)
+	}
+	return nil
+}
+
+func checkLowercase(s string) error {
+	for _, c := range s {
+		if !unicode.IsLower(c) && unicode.IsLetter(c) {
+			return fmt.Errorf("%s: only lowercase letters allowed", s)
+		}
+	}
+	return nil
+}
+
+func handleNamingConventions(s string, cfg config.Conventions) error {
+	var errs []error
+	if len(cfg.Naming) > 0 {
+		for _, nameing := range cfg.Naming {
+			if nameing == "no_spaces" {
+				errs = append(errs, checkNoSpace(s))
+			}
+			if nameing == "lowercase" {
+				errs = append(errs, checkLowercase(s))
 			}
 		}
 	}
