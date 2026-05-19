583500b42e559b88042e5214c193a3637acaddd6
Asher Samuel <ashersam116@gmail.com>
2026-05-19 20:10:03 +0300

    generate default .gitlang.yaml

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -67,11 +67,95 @@ exec ./.gbin/dispatcher %s "$@"`, hook)
 }
 
 func generateConfigFile() error {
-	// check if config file exists
-	// if exists, don't replace
-	// make file
-	// populate config content from example config
-	return nil
+	file, err := os.OpenFile(".gitlang.yaml", os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0666)
+	if err != nil {
+		return err
+	}
+
+	// default config
+	content := []byte(`# ==========================================
+# This is default generate config file by gitlang cli
+# Feel free to change the config as you wish
+# ==========================================
+
+# ------------------------------------------
+# Branch Rules
+# ------------------------------------------
+branch:
+  enabled: true # If false, branch validation is skipped
+
+  # List of branches that cannot receive direct commits.
+  # Commits on these branches will fail.
+  protected:
+    - main
+    - test
+
+  # Regex pattern that branch names must follow (excluding master/main)
+  pattern: "^(feature|bugfix|hotfix)/[A-Z]+-[0-9]+"
+
+# ------------------------------------------
+# Pre-Commit Rules
+# ------------------------------------------
+pre-commit:
+  enabled: true # If false, pre-commit checks are skipped
+
+  # Maximum allowed file size (in KB) for staged files
+  # Fails larger than this will cause commit to fail
+  max_file_size_kb: 5000
+
+  # File extendsions that are not allowed to be commited
+  forbidden_extensions: 
+    - .log
+    - .tmp
+    - .swp
+    # in unix system executable are considered to have .exe extension
+    # so to skip exectables just specify .exe
+    - .exe
+
+  naming_conventions:
+    folder:
+      naming:
+        - no_spaces # Folder names cannot contain spaces
+        - lowercase # Folder names must be lowercase only
+    file:
+      naming:
+        - no_spaces # File names cannot contain spaces
+        - lowercase # File names must be lowercase only
+
+  # Command to run before commit
+  # These commands only execute if staged files match the given patterns
+  run:
+    # Run ESLint only if staged JS or TS files exist
+    - cmd: "npm run lint"
+      match:
+        - "*.js"
+        - "*.ts"
+
+    # Run go vet only if staged Go files exist
+    - cmd: "go vet ./..."
+      match:
+        - "*.go"
+
+# ------------------------------------------
+# Commit Message Rules
+# ------------------------------------------
+commit-msg:
+  enabled: true # If false, commit message validation is skipped
+  min_length: 10 # Minimum number of characters required in commit message
+
+  # commit message must NOT end with a period
+  # invalid example: "fix login bug."
+  no_trailing_period: true
+
+  # Words thats are not allowed inside commit messages
+  # Matching is case-insensitive
+  forbidden_words:
+    - tmp
+    - stuff
+    - wip
+`)
+	_, err = file.Write(content)
+	return err
 }
 
 func main() {
